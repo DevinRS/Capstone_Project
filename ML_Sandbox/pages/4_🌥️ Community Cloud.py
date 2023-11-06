da5292e8a7cb751c03f9376973fd1e316ccc414c
AurHubertMax <112564421+AurHubertMax@users.noreply.github.com>
2023-11-05 21:57:49 -0600

    Update 4_🌥️ Community Cloud.py
    Error handling and disable multiple MLnames uploaded on the database

--- ML_Sandbox/pages/4_🌥️ Community Cloud.py
+++ ML_Sandbox/pages/4_🌥️ Community Cloud.py
@@ -7,6 +7,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.types import PickleType
 import datetime
+import base64
+import streamlit.components.v1 as components
 
 # ----
 # Page Config
@@ -53,6 +55,7 @@ Base.metadata.create_all(bind=engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+
 # ----
 # Title of the Page
 # ----
@@ -77,30 +80,94 @@ st.markdown('<p class="page-title">Community Cloud</p>', unsafe_allow_html=True)
 st.divider()
 st.header('Create a Post', anchor=None)
 
+# ----
+# Display Models Def
+# ----
+def display_models():
+    for model in results:
+        st.subheader(model.MLname, anchor=False, divider=False)
+        st.write(model.description)
+        MLlongDesc = model.longDescription
+        st.expander('Read More').write(MLlongDesc)
+        col1, col2, col3, col4, col5 = st.columns(5)
+        with col1:
+            st.caption(model.uploadTime)
+        with col2:
+            st.caption(ratingToStars(model.rating))
+        
+        with col5:
+            download_button_label = f"{model.MLname}.pkl"
+            file_name = f"{model.MLname}.pkl"
+            binaryData = pickle.dumps(model.item)
+            binaryDataBytes = bytes(binaryData)
+
+            button_html = f'''
+                <style>
+                    .download-button {{
+                        text-decoration: none;
+                        padding: 5px 10px;
+                        border-radius: 50px;
+                        text-align: center;
+                        color: white;
+                        background-color: #ff6e1c;
+                        border: solid 1px #ffda89;
+                        font-family: 'Roboto', sans-serif;
+                        transition: 0.3s;
+                    }}
+                    .download-button:hover {{
+                        background-color: #ffd884;
+                        color: #ff6e1c;
+                        border: solid 1px #ff6e1c;
+                    }}
+                </style>
+                <a href="data:application/octet-stream;base64,{base64.b64encode(binaryDataBytes).decode()}"
+                download="{file_name}" class="download-button">
+                    {download_button_label}
+                </a>
+            '''
+            components.html(button_html, height=50)
+        
+        st.divider()
+
+# change rating from ints to stars
+def ratingToStars(rating):
+    stars = "⭐" * rating
+    return stars
+
 
 # ----
 # Create Post
 # ----
-
 with st.form(key="uploadModelForm", clear_on_submit=True):
     modelName = st.text_input(label='Model Name:')
     modelDescription = st.text_area(label='Short Description of Model', 
                                     placeholder='e.g.  probabilistic machine learning algorithm')
     modelLongDesc = st.text_area(label='Detailed Description of Model', 
-                                 placeholder='e.g. Naïve Bayes is a probabilistic machine learning algorithm used for many classification functions and is based on the Bayes theorem. Gaussian Naïve Bayes is the extension of naïve Bayes. While other functions are used to estimate data distribution, Gaussian or normal distribution is the simplest to implement as you will need to calculate the mean and standard deviation for the training data.')
+                                placeholder='e.g. Naïve Bayes is a probabilistic machine learning algorithm used for many classification functions and is based on the Bayes theorem. Gaussian Naïve Bayes is the extension of naïve Bayes. While other functions are used to estimate data distribution, Gaussian or normal distribution is the simplest to implement as you will need to calculate the mean and standard deviation for the training data.')
     modelItem = st.file_uploader(label='Upload Model 😊', type = ['pkl', 'pickle'], key = 'model_file')
     modelRating = 4
     uploadButton = st.form_submit_button(label='Upload Your Post')
     if (uploadButton == True):
         if (len(modelName) == 0):
-            st.warning('Put something in')
-            st.rerun()
-        modelTime = st.date_input(label='',value=datetime.datetime.now(), format='MM/DD/YYYY')
-        model = Models(modelName, modelDescription, modelLongDesc, modelItem, modelRating, modelTime) 
-        session.add(model)
-        session.commit()
-        st.success(f'Model "{modelItem.name}" has been successfully uploaded!')
-        st.rerun()
+            st.warning('Please enter a model name')
+        else:
+            existing_model = session.query(Models).filter(Models.MLname == modelName).first()
+
+            if existing_model:
+                st.warning(f'Model name "{modelName}" already exists. Please choose a different name')
+            
+            else:
+                if modelItem == None:
+                    st.warning('Please upload a file for your model.')
+                else:
+
+                    modelTime = datetime.datetime.now().strftime('%m/%d/%Y')
+                    
+                    model = Models(modelName, modelDescription, modelLongDesc, modelItem, modelRating, modelTime) 
+                    session.add(model)
+                    session.commit()
+                    st.success(f'Model "{modelItem.name}" has been successfully uploaded!')
+                    st.rerun()
 
 
 #creates random ML models
@@ -109,17 +176,21 @@ import os
 fake = Faker()
 
 os.makedirs('pickle_files', exist_ok=True)
-
+generated_names = set()
 createRandomMls = st.button('create random MLs', type='primary')
 if (createRandomMls):
-    
     for i in range(10):
-        
-        randomName = fake.first_name()
+        while True:
+            randomName = fake.first_name()
+
+            if randomName not in generated_names:
+                generated_names.add(randomName)
+                break
+
         randomDesc = fake.text()
         randomLongDesc = fake.text()
         randomRating = fake.random_int(min=1, max=5)
-        randomUploadTime = fake.date_time_this_decade()
+        randomUploadTime = fake.date_time_this_decade().strftime('%m/%d/%Y')
 
         random_data = {
             'name': randomName,
@@ -127,12 +198,13 @@ if (createRandomMls):
             'rating': randomRating
         }
 
-        pickle_filename=f'pickle_files/random_data_{i}.pkl'
+        pickle_filename = f'pickle_files/random_data{i}.pkl'
         with open(pickle_filename, 'wb') as file:
             pickle.dump(random_data, file)
-
+        
         with open(pickle_filename, 'rb') as file:
             loaded_data = pickle.load(file)
+        modelTime = randomUploadTime
 
         randomModel = Models(randomName, randomDesc, randomLongDesc, loaded_data, randomRating, randomUploadTime)
         session.add(randomModel)
@@ -152,14 +224,11 @@ if (clearModels):
 # Filter displays
 # ----
 st.header('Community Posts')
-import base64
-import streamlit.components.v1 as components
 
 
-# change rating from ints to stars
-def ratingToStars(rating):
-    stars = "⭐" * rating
-    return stars
+
+
+
 
 # ----
 # Search Bar
@@ -222,6 +291,7 @@ with st.form(key='searchForm', clear_on_submit=True):
                 st.divider()
 
 
+
 # ----
 # Filtered Results
 # ----
@@ -229,142 +299,13 @@ with st.container():
     tab1, tab2, tab3 = st.tabs(['Alphabetical', 'Newest', 'Rating'])
     with tab1:
         results = session.query(Models).order_by(Models.MLname).all()
-        for model in results:
-            st.subheader(model.MLname, anchor=False, divider=False)
-            st.write(model.description)
-            MLlongDesc = model.longDescription
-            st.expander('Read More').write(MLlongDesc)
-            col1, col2, col3, col4, col5 = st.columns(5)
-            with col1:
-                st.caption(model.uploadTime)
-            with col2:
-                st.caption(ratingToStars(model.rating))
-            
-            with col5:
-                download_button_label = f"{model.MLname}.pkl"
-                file_name = f"{model.MLname}.pkl"
-                binaryData = pickle.dumps(model.item)
-                binaryDataBytes = bytes(binaryData)
-
-                button_html = f'''
-                    <style>
-                        .download-button {{
-                            text-decoration: none;
-                            padding: 5px 10px;
-                            border-radius: 50px;
-                            text-align: center;
-                            color: white;
-                            background-color: #ff6e1c;
-                            border: solid 1px #ffda89;
-                            font-family: 'Roboto', sans-serif;
-                            transition: 0.3s;
-                        }}
-                        .download-button:hover {{
-                            background-color: #ffd884;
-                            color: #ff6e1c;
-                            border: solid 1px #ff6e1c;
-                        }}
-                    </style>
-                    <a href="data:application/octet-stream;base64,{base64.b64encode(binaryDataBytes).decode()}"
-                    download="{file_name}" class="download-button">
-                        {download_button_label}
-                    </a>
-                '''
-                components.html(button_html, height=50)
-            
-            st.divider()
+        display_models()
 
     with tab2:
         results = session.query(Models).order_by(Models.uploadTime.desc()).all()
-        for model in results:
-            st.subheader(model.MLname, anchor=False, divider=False)
-            st.write(model.description)
-            MLlongDesc = model.longDescription
-            st.expander('Read More').write(MLlongDesc)
-            col1, col2, col3, col4, col5 = st.columns(5)
-            with col1:
-                st.caption(model.uploadTime)
-            with col2:
-                st.caption(ratingToStars(model.rating))
-            
-            with col5:
-                download_button_label = f"{model.MLname}.pkl"
-                file_name = f"{model.MLname}.pkl"
-                binaryData = pickle.dumps(model.item)
-                binaryDataBytes = bytes(binaryData)
-
-                button_html = f'''
-                    <style>
-                        .download-button {{
-                            text-decoration: none;
-                            padding: 5px 10px;
-                            border-radius: 50px;
-                            text-align: center;
-                            color: white;
-                            background-color: #ff6e1c;
-                            border: solid 1px #ffda89;
-                            font-family: 'Roboto', sans-serif;
-                            transition: 0.3s;
-                        }}
-                        .download-button:hover {{
-                            background-color: #ffd884;
-                            color: #ff6e1c;
-                            border: solid 1px #ff6e1c;
-                        }}
-                    </style>
-                    <a href="data:application/octet-stream;base64,{base64.b64encode(binaryDataBytes).decode()}"
-                    download="{file_name}" class="download-button">
-                        {download_button_label}
-                    </a>
-                '''
-                components.html(button_html, height=50)
-            
-            st.divider()
+        display_models()
             
     with tab3:
         results = session.query(Models).order_by(Models.rating.desc()).all()
-        for model in results:
-            st.subheader(model.MLname, anchor=False, divider=False)
-            st.write(model.description)
-            MLlongDesc = model.longDescription
-            st.expander('Read More').write(MLlongDesc)
-            col1, col2, col3, col4, col5 = st.columns(5)
-            with col1:
-                st.caption(model.uploadTime)
-            with col2:
-                st.caption(ratingToStars(model.rating))
-            
-            with col5:
-                download_button_label = f"{model.MLname}.pkl"
-                file_name = f"{model.MLname}.pkl"
-                binaryData = pickle.dumps(model.item)
-                binaryDataBytes = bytes(binaryData)
-
-                button_html = f'''
-                    <style>
-                        .download-button {{
-                            text-decoration: none;
-                            padding: 5px 10px;
-                            border-radius: 50px;
-                            text-align: center;
-                            color: white;
-                            background-color: #ff6e1c;
-                            border: solid 1px #ffda89;
-                            font-family: 'Roboto', sans-serif;
-                            transition: 0.3s;
-                        }}
-                        .download-button:hover {{
-                            background-color: #ffd884;
-                            color: #ff6e1c;
-                            border: solid 1px #ff6e1c;
-                        }}
-                    </style>
-                    <a href="data:application/octet-stream;base64,{base64.b64encode(binaryDataBytes).decode()}"
-                    download="{file_name}" class="download-button">
-                        {download_button_label}
-                    </a>
-                '''
-                components.html(button_html, height=50)
-            
-            st.divider()
+        display_models()
         
