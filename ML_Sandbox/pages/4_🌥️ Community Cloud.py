432d464640ed1ca2d3fc3453fdbbae2545643b0d
AurHubertMax <112564421+AurHubertMax@users.noreply.github.com>
2023-12-03 21:35:23 -0600

    pkl to json

--- ML_Sandbox/pages/4_🌥️ Community Cloud.py
+++ ML_Sandbox/pages/4_🌥️ Community Cloud.py
@@ -9,6 +9,7 @@ from sqlalchemy.types import PickleType
 import datetime
 import base64
 import streamlit.components.v1 as components
+import json
 
 # ----
 # Page Config
@@ -80,54 +81,7 @@ st.markdown('<p class="page-title">Community Cloud</p>', unsafe_allow_html=True)
 st.divider()
 st.header('Create a Post', anchor=None)
 
-# ----
-# Display Models Def
-# ----
-def display_models():
-    for model in results:
-        st.subheader(model.MLname, anchor=False, divider=False)
-        st.write(model.description)
-        MLlongDesc = model.longDescription
-        st.expander('Read More').write(MLlongDesc)
-        col1, col2, col3, col4, col5 = st.columns(5)
-        with col1:
-            st.caption(model.uploadTime)
-        with col2:
-            st.caption(ratingToStars(model.rating))
-        
-        with col5:
-            download_button_label = f"{model.MLname}.pkl"
-            file_name = f"{model.MLname}.pkl"
-            binaryData = pickle.dumps(model.item)
-            binaryDataBytes = bytes(binaryData)
 
-            button_html = f'''
-                <style>
-                    .download-button {{
-                        text-decoration: none;
-                        padding: 5px 10px;
-                        border-radius: 50px;
-                        text-align: center;
-                        color: white;
-                        background-color: #ff6e1c;
-                        border: solid 1px #ffda89;
-                        font-family: 'Roboto', sans-serif;
-                        transition: 0.3s;
-                    }}
-                    .download-button:hover {{
-                        background-color: #ffd884;
-                        color: #ff6e1c;
-                        border: solid 1px #ff6e1c;
-                    }}
-                </style>
-                <a href="data:application/octet-stream;base64,{base64.b64encode(binaryDataBytes).decode()}"
-                download="{file_name}" class="download-button">
-                    {download_button_label}
-                </a>
-            '''
-            components.html(button_html, height=50)
-        
-        st.divider()
 
 # change rating from ints to stars
 def ratingToStars(rating):
@@ -166,6 +120,9 @@ with st.form(key="uploadModelForm", clear_on_submit=True):
                     model = Models(modelName, modelDescription, modelLongDesc, modelItem, modelRating, modelTime) 
                     session.add(model)
                     session.commit()
+
+                    
+
                     st.success(f'Model "{modelItem.name}" has been successfully uploaded!')
                     st.rerun()
 
@@ -176,6 +133,7 @@ import os
 fake = Faker()
 
 os.makedirs('pickle_files', exist_ok=True)
+os.makedirs('json_files', exist_ok=True)
 generated_names = set()
 createRandomMls = st.button('create random MLs', type='primary')
 if (createRandomMls):
@@ -198,15 +156,25 @@ if (createRandomMls):
             'rating': randomRating
         }
 
+        #Save pickle file
         pickle_filename = f'pickle_files/random_data{i}.pkl'
         with open(pickle_filename, 'wb') as file:
             pickle.dump(random_data, file)
         
-        with open(pickle_filename, 'rb') as file:
-            loaded_data = pickle.load(file)
-        modelTime = randomUploadTime
+        #save json file
+        json_filename = f'json_files/random_data{i}.json'
+        with open(json_filename, 'w') as file:
+            json.dump(random_data, file, indent=2)
 
-        randomModel = Models(randomName, randomDesc, randomLongDesc, loaded_data, randomRating, randomUploadTime)
+        randomModel = Models(
+            MLname=random_data['name'],
+            description=random_data['desc'],
+            longDescription=randomLongDesc,
+            item=random_data,
+            rating=random_data['rating'],
+            uploadTime=randomUploadTime
+        )
+
         session.add(randomModel)
         session.commit()
     st.success('Created 10 random models')
@@ -226,6 +194,104 @@ if (clearModels):
 st.header('Community Posts')
 
 
+# ----
+# Display Models Def
+# ----
+def display_models():
+    st.subheader(model.MLname, anchor=False, divider=False)
+    st.write(model.description)
+    MLlongDesc = model.longDescription
+    st.expander('Read More').write(MLlongDesc)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.caption(model.uploadTime)
+    with col2:
+        st.caption(ratingToStars(model.rating))
+    
+    with col5:
+        # Download button for pickle format
+        download_button_label = f"{model.MLname}.pkl"
+        file_name = f"{model.MLname}.pkl"
+        binaryData = pickle.dumps(model.item)
+        binaryDataBytes = bytes(binaryData)
+
+        
+        button_html = f'''
+            <style>
+                .download-button {{
+                    text-decoration: none;
+                    padding: 5px 10px;
+                    border-radius: 50px;
+                    text-align: center;
+                    color: white;
+                    background-color: #ff6e1c;
+                    border: solid 1px #ffda89;
+                    font-family: 'Roboto', sans-serif;
+                    transition: 0.3s;
+                }}
+                .download-button:hover {{
+                    background-color: #ffd884;
+                    color: #ff6e1c;
+                    border: solid 1px #ff6e1c;
+                }}
+            </style>
+            <a href="data:application/octet-stream;base64,{base64.b64encode(binaryDataBytes).decode()}"
+            download="{file_name}" class="download-button">
+                {download_button_label}
+            </a>
+        '''
+        components.html(button_html, height=50)
+
+        # Download Button for JSON format
+        obj_pkl = model.item
+        json_obj = json.loads(json.dumps(obj_pkl, default=str))
+
+        post_data = {
+            "type": "post",
+            "id": model.MLname,
+            "model": [
+                {"modelName": model.MLname},
+                {"modelDescription": model.description},
+                {"modelLongDescription": model.longDescription},
+                {"modelRating": model.rating},
+                {"modelUploadTime": model.uploadTime},
+                {"modelItem": json_obj}
+            ]
+        }
+
+        download_button_label_json = f"{model.MLname}.json"
+        file_name_json = f"{model.MLname}.json"
+        json_data = json.dumps(post_data, indent=2).encode('utf-8')
+        binaryDataBytes_json = bytes(json_data)
+        
+
+        button_html_json = f'''
+            <style>
+                .download-button {{
+                    text-decoration: none;
+                    padding: 5px 10px;
+                    border-radius: 50px;
+                    text-align: center;
+                    color: white;
+                    background-color: #2ecc71; /* Change color for JSON button */
+                    border: solid 1px #27ae60; /* Change border color for JSON button */
+                    font-family: 'Roboto', sans-serif;
+                    transition: 0.3s;
+                }}
+                .download-button:hover {{
+                    background-color: #2ecc71; /* Change hover background color for JSON button */
+                    color: #27ae60; /* Change hover color for JSON button */
+                    border: solid 1px #27ae60; /* Change hover border color for JSON button */
+                }}
+            </style>
+            <a href="data:application/octet-stream;base64,{base64.b64encode(binaryDataBytes_json).decode()}"
+            download="{file_name_json}" class="download-button">
+                {download_button_label_json}
+            </a>
+        '''
+        components.html(button_html_json, height=50)
+    
+    st.divider()
 
 
 
@@ -246,49 +312,7 @@ with st.form(key='searchForm', clear_on_submit=True):
             st.warning('No matching results found.')
         else:
             for model in filteredResults:
-                st.subheader(model.MLname, anchor=False, divider=False)
-                st.write(model.description)
-                MLlongDesc = model.longDescription
-                st.expander('Read More').write(MLlongDesc)
-                col1, col2, col3, col4, col5 = st.columns(5)
-                with col1:
-                    st.caption(model.uploadTime)
-                with col2:
-                    st.caption(ratingToStars(model.rating))
-                
-                with col5:
-                    download_button_label = f"{model.MLname}.pkl"
-                    file_name = f"{model.MLname}.pkl"
-                    binaryData = pickle.dumps(model.item)
-                    binaryDataBytes = bytes(binaryData)
-
-                    button_html = f'''
-                        <style>
-                            .download-button {{
-                                text-decoration: none;
-                                padding: 5px 10px;
-                                border-radius: 50px;
-                                text-align: center;
-                                color: white;
-                                background-color: #ff6e1c;
-                                border: solid 1px #ffda89;
-                                font-family: 'Roboto', sans-serif;
-                                transition: 0.3s;
-                            }}
-                            .download-button:hover {{
-                                background-color: #ffd884;
-                                color: #ff6e1c;
-                                border: solid 1px #ff6e1c;
-                            }}
-                        </style>
-                        <a href="data:application/octet-stream;base64,{base64.b64encode(binaryDataBytes).decode()}"
-                        download="{file_name}" class="download-button">
-                            {download_button_label}
-                        </a>
-                    '''
-                    components.html(button_html, height=50)
-                
-                st.divider()
+                display_models()
 
 
 
@@ -299,13 +323,16 @@ with st.container():
     tab1, tab2, tab3 = st.tabs(['Alphabetical', 'Newest', 'Rating'])
     with tab1:
         results = session.query(Models).order_by(Models.MLname).all()
-        display_models()
+        for model in results:
+            display_models()
 
     with tab2:
         results = session.query(Models).order_by(Models.uploadTime.desc()).all()
-        display_models()
+        for model in results:
+            display_models()
             
     with tab3:
         results = session.query(Models).order_by(Models.rating.desc()).all()
-        display_models()
+        for model in results:
+            display_models()
         
