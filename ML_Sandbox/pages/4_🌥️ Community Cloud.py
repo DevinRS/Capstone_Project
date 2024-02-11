03825e471b91b3f360128a4db9c23d54a4eb6a19
dangnick <58992608+dangnick@users.noreply.github.com>
2024-02-11 14:53:52 -0600

    Update database attributes for 4_🌥️ Community Cloud.py

--- ML_Sandbox/pages/4_🌥️ Community Cloud.py
+++ ML_Sandbox/pages/4_🌥️ Community Cloud.py
@@ -2,6 +2,9 @@ import string
 import streamlit as st
 from streamlit_option_menu import option_menu
 import hydralit_components as hc
+import yaml
+from yaml.loader import SafeLoader
+
 
 # ----
 # Page Config
@@ -14,6 +17,7 @@ st.set_page_config(
     menu_items=None
 )
 
+
 with hc.HyLoader('Loading Community Cloud',hc.Loaders.standard_loaders,index=5):
     import pickle
     from sqlalchemy import create_engine, Column, Integer, String, DateTime
@@ -25,15 +29,6 @@ with hc.HyLoader('Loading Community Cloud',hc.Loaders.standard_loaders,index=5):
     import streamlit.components.v1 as components
     import json
 
-# ----
-# Topbar
-# ----
-selected = option_menu(
-    menu_title= "Community Cloud",
-    options=[''],
-    orientation="horizontal",
-    menu_icon="cloud-sun",
-)
 
 if 'authentication_status' not in st.session_state:
     st.session_state['authentication_status'] = None
@@ -58,26 +53,29 @@ with st.sidebar:
 Base = declarative_base()
 class Models(Base):
     #set table name and columns
-    __tablename__ = "models"
-    MLname = Column("name", String, primary_key=True)
+    post_id = Column(Integer, primary_key=True, autoincrement=True)
+    __tablename__ = "posts"
+    MLname = Column("name", String)
+    post_owner = Column("owner", String)
     description = Column("description", String)
     longDescription = Column("longDesc", String)
-    # downloads = Column("downloads", Integer)
     item = Column("item", PickleType)
-    rating = Column("rating", Integer)
+    numLikes = Column("likes", Integer)
+    numDislikes = Column("dislikes", Integer)
     uploadTime = Column("date", String)
 
-    def __init__ (self, MLname, description, longDescription, item, rating, uploadTime): #, rating, downloads, uploadTime):
+    def __init__ (self, MLname, post_owner, description, longDescription, item, numLikes, numDislikes, uploadTime):
         self.MLname = MLname
+        self.post_owner = post_owner
         self.description = description
         self.longDescription = longDescription
-        # self.downloads = downloads
         self.item = item
-        self.rating = rating
+        self.numLikes = numLikes
+        self.numDislikes = numDislikes
         self.uploadTime = uploadTime
     
-    def __repr__ (self):
-        return f"{self.MLname}, {self.description}, {self.longDescription}, {self.item.name}, {self.rating}, {self.uploadTime}"
+    def __repr__ (self):   
+            return f"{self.MLname}, {self.description}, {self.longDescription}, {self.item}, {self.numLikes}, {self.numDislikes}, {self.uploadTime}"
 
 
 engine = create_engine("sqlite:///file_uploads.db")
@@ -111,104 +109,46 @@ st.markdown('<p class="page-title">Community Cloud</p>', unsafe_allow_html=True)
 st.divider()
 st.header('Create a Post', anchor=None)
 
-
-
-# change rating from ints to stars
-def ratingToStars(rating):
-    stars = "⭐" * rating
-    return stars
-
-
-# ----
-# Create Post
-# ----
-with st.form(key="uploadModelForm", clear_on_submit=True):
-    modelName = st.text_input(label='Model Name:')
-    modelDescription = st.text_area(label='Short Description of Model', 
-                                    placeholder='e.g.  probabilistic machine learning algorithm')
-    modelLongDesc = st.text_area(label='Detailed Description of Model', 
-                                placeholder='e.g. Naïve Bayes is a probabilistic machine learning algorithm used for many classification functions and is based on the Bayes theorem. Gaussian Naïve Bayes is the extension of naïve Bayes. While other functions are used to estimate data distribution, Gaussian or normal distribution is the simplest to implement as you will need to calculate the mean and standard deviation for the training data.')
-    modelItem = st.file_uploader(label='Upload Model 😊', type = ['pkl', 'pickle'], key = 'model_file')
-    modelRating = 4
-    uploadButton = st.form_submit_button(label='Upload Your Post')
-    if (uploadButton == True):
-        if (len(modelName) == 0):
-            st.warning('Please enter a model name')
-        else:
-            existing_model = session.query(Models).filter(Models.MLname == modelName).first()
-
-            if existing_model:
-                st.warning(f'Model name "{modelName}" already exists. Please choose a different name')
-            
-            else:
-                if modelItem == None:
-                    st.warning('Please upload a file for your model.')
-                else:
-
-                    modelTime = datetime.datetime.now().strftime('%m/%d/%Y')
-                    
-                    model = Models(modelName, modelDescription, modelLongDesc, modelItem, modelRating, modelTime) 
-                    session.add(model)
-                    session.commit()
-
-                    
-
-                    st.success(f'Model "{modelItem.name}" has been successfully uploaded!')
-                    st.rerun()
-
-
-#creates random ML models
-from faker import Faker
-import os
-fake = Faker()
-
-os.makedirs('pickle_files', exist_ok=True)
-os.makedirs('json_files', exist_ok=True)
-generated_names = set()
-createRandomMls = st.button('create random MLs', type='primary')
-if (createRandomMls):
-    for i in range(10):
-        while True:
-            randomName = fake.first_name()
-
-            if randomName not in generated_names:
-                generated_names.add(randomName)
-                break
-
-        randomDesc = fake.text()
-        randomLongDesc = fake.text()
-        randomRating = fake.random_int(min=1, max=5)
-        randomUploadTime = fake.date_time_this_decade().strftime('%m/%d/%Y')
-
-        random_data = {
-            'name': randomName,
-            'desc': randomDesc,
-            'rating': randomRating
-        }
-
-        #Save pickle file
-        pickle_filename = f'pickle_files/random_data{i}.pkl'
-        with open(pickle_filename, 'wb') as file:
-            pickle.dump(random_data, file)
+with open('users.yaml') as f:
+    users = yaml.load(f, Loader=SafeLoader)
+    
+    # Create a post
+    with st.form(key="uploadModelForm", clear_on_submit=True):
+        post_owner = st.session_state['username']
         
-        #save json file
-        json_filename = f'json_files/random_data{i}.json'
-        with open(json_filename, 'w') as file:
-            json.dump(random_data, file, indent=2)
+        modelName = st.text_input(label='Model Name:')
+        modelDescription = st.text_area(label='Short Description of Model', 
+                                        placeholder='e.g.  probabilistic machine learning algorithm')
+        modelLongDesc = st.text_area(label='Detailed Description of Model', 
+                                    placeholder='e.g. Naïve Bayes is a probabilistic machine learning algorithm used for many classification functions and is based on the Bayes theorem. Gaussian Naïve Bayes is the extension of naïve Bayes. While other functions are used to estimate data distribution, Gaussian or normal distribution is the simplest to implement as you will need to calculate the mean and standard deviation for the training data.')
+        modelItem = st.file_uploader(label='Upload Model', type = ['pkl', 'pickle'], key = 'model_file')
+        modelNumLikes = 0
+        modelNumDislikes = 0
+        uploadButton = st.form_submit_button(label='Upload Your Post')
+        modelTime = datetime.datetime.now().strftime('%m/%d/%Y')
+        if (uploadButton == True):
+            if (len(modelName) == 0):
+                st.warning('Please enter a model name')
+            else:
+                existing_model = session.query(Models).filter(Models.MLname == modelName).first()
 
-        randomModel = Models(
-            MLname=random_data['name'],
-            description=random_data['desc'],
-            longDescription=randomLongDesc,
-            item=random_data,
-            rating=random_data['rating'],
-            uploadTime=randomUploadTime
-        )
-
-        session.add(randomModel)
-        session.commit()
-    st.success('Created 10 random models')
-
+                if existing_model:
+                    st.warning(f'Model name "{modelName}" already exists. Please choose a different name')
+                else:
+                    if modelItem == None:
+                        st.warning('Please upload a file for your model.')
+                    else:
+                        
+                        
+                        model = Models(modelName, post_owner, modelDescription, modelLongDesc, modelItem, modelNumLikes, modelNumDislikes, modelTime) 
+                        session.add(model)
+                        session.commit()
+                        
+                        st.success(f'Model "{modelItem.name}" has been successfully uploaded!')
+                        st.rerun()
+        
+    
+    
 #clears all ML models
 clearModels = st.button('clear Models', key='clearModels', type='primary')
 if (clearModels):
@@ -217,152 +157,22 @@ if (clearModels):
     st.success('All models have been deleted')
 
 
-
-# ----
-# Filter displays
-# ----
-st.header('Community Posts')
-
-
-# ----
-# Display Models Def
-# ----
 def display_models():
-    st.subheader(model.MLname, anchor=False, divider=False)
-    st.write(model.description)
-    MLlongDesc = model.longDescription
-    st.expander('Read More').write(MLlongDesc)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.caption(model.uploadTime)
-    with col2:
-        st.caption(ratingToStars(model.rating))
+    models = session.query(Models).all()
+
+    if not models:
+        st.info("No Models bitch")
     
-    with col5:
-        # Download button for pickle format
-        download_button_label = f"{model.MLname}.pkl"
-        file_name = f"{model.MLname}.pkl"
-        binaryData = pickle.dumps(model.item)
-        binaryDataBytes = bytes(binaryData)
+    else:
+        for model in models:
+            st.subheader(model.MLname)
+            st.write(f"Post id: {model.post_id}")
+            st.write(f"Uploaded by: {model.post_owner}")
+            st.write(f"Description: {model.description}")
+            st.write(f"Detailed Description: {model.longDescription}")
+            st.write(f"Number of Likes: {model.numLikes}")
+            st.write(f"Number of Dislikes: {model.numDislikes}")
+            st.write(f"Upload Time: {model.uploadTime}")
+            st.markdown("---")
 
-        
-        button_html = f'''
-            <style>
-                .download-button {{
-                    text-decoration: none;
-                    padding: 5px 10px;
-                    border-radius: 50px;
-                    text-align: center;
-                    color: white;
-                    background-color: #ff6e1c;
-                    border: solid 1px #ffda89;
-                    font-family: 'Roboto', sans-serif;
-                    transition: 0.3s;
-                }}
-                .download-button:hover {{
-                    background-color: #ffd884;
-                    color: #ff6e1c;
-                    border: solid 1px #ff6e1c;
-                }}
-            </style>
-            <a href="data:application/octet-stream;base64,{base64.b64encode(binaryDataBytes).decode()}"
-            download="{file_name}" class="download-button">
-                {download_button_label}
-            </a>
-        '''
-        components.html(button_html, height=50)
-
-        # Download Button for JSON format
-        obj_pkl = model.item
-        json_obj = json.loads(json.dumps(obj_pkl, default=str))
-
-        post_data = {
-            "type": "post",
-            "id": model.MLname,
-            "model": [
-                {"modelName": model.MLname},
-                {"modelDescription": model.description},
-                {"modelLongDescription": model.longDescription},
-                {"modelRating": model.rating},
-                {"modelUploadTime": model.uploadTime},
-                {"modelItem": json_obj}
-            ]
-        }
-
-        download_button_label_json = f"{model.MLname}.json"
-        file_name_json = f"{model.MLname}.json"
-        json_data = json.dumps(post_data, indent=2).encode('utf-8')
-        binaryDataBytes_json = bytes(json_data)
-        
-
-        button_html_json = f'''
-            <style>
-                .download-button {{
-                    text-decoration: none;
-                    padding: 5px 10px;
-                    border-radius: 50px;
-                    text-align: center;
-                    color: white;
-                    background-color: #2ecc71; /* Change color for JSON button */
-                    border: solid 1px #27ae60; /* Change border color for JSON button */
-                    font-family: 'Roboto', sans-serif;
-                    transition: 0.3s;
-                }}
-                .download-button:hover {{
-                    background-color: #2ecc71; /* Change hover background color for JSON button */
-                    color: #27ae60; /* Change hover color for JSON button */
-                    border: solid 1px #27ae60; /* Change hover border color for JSON button */
-                }}
-            </style>
-            <a href="data:application/octet-stream;base64,{base64.b64encode(binaryDataBytes_json).decode()}"
-            download="{file_name_json}" class="download-button">
-                {download_button_label_json}
-            </a>
-        '''
-        components.html(button_html_json, height=50)
-    
-    st.divider()
-
-
-
-
-# ----
-# Search Bar
-# ----
-with st.form(key='searchForm', clear_on_submit=True):
-
-    searchQuery = st.text_input(' Search Posts', '', placeholder='🔍 Search Community Posts', )
-
-    results = session.query(Models).all()
-    filteredResults = [model for model in results if searchQuery.lower() in model.MLname.lower()]
-    searchButton = st.form_submit_button(label='search')
-
-    if (searchQuery):
-        if (not filteredResults):
-            st.warning('No matching results found.')
-        else:
-            for model in filteredResults:
-                display_models()
-
-
-
-# ----
-# Filtered Results
-# ----
-with st.container():
-    tab1, tab2, tab3 = st.tabs(['Alphabetical', 'Newest', 'Rating'])
-    with tab1:
-        results = session.query(Models).order_by(Models.MLname).all()
-        for model in results:
-            display_models()
-
-    with tab2:
-        results = session.query(Models).order_by(Models.uploadTime.desc()).all()
-        for model in results:
-            display_models()
-            
-    with tab3:
-        results = session.query(Models).order_by(Models.rating.desc()).all()
-        for model in results:
-            display_models()
-        
+display_models()
